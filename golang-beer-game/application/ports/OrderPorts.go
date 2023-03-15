ac696b170a767c990d4bfb2dd963cd4366e9c337
LeonFelipeCordero <zolopretroriano@gmail.com>
2023-03-15 07:08:10 +0100

    chore: delete useless files

--- golang-beer-game/application/ports/OrderPorts.go
+++ golang-beer-game/application/ports/OrderPorts.go
@@ -9,25 +9,15 @@ import (
 type IOrderApi interface {
 	CreateOrder(ctx context.Context, receiverId string) (*model.Order, error)
 	DeliverOrder(ctx context.Context, orderId string, amount int) (*model.Order, error)
-	UpdateWeeklyOrder(ctx context.Context, name string) (*model.Board, error)
-	GetAvailableRoles(ctx context.Context, id string) ([]*model.Role, error)
-	GetByPlayer(ctx context.Context, playerId string) (*model.Board, error)
 }
 
 type IOrderService interface {
-	Create(ctx context.Context, name string) (*domain.Board, error)
-	Get(ctx context.Context, id string) (*domain.Board, error)
-	GetByName(ctx context.Context, name string) (*domain.Board, error)
-	GetByPlayer(ctx context.Context, playerId string) (*domain.Board, error)
-	CompleteBoard(ctx context.Context, id string) error
-	GetAvailableRoles(ctx context.Context, id string) ([]domain.Role, error)
+	CreateOrder(ctx context.Context, receiverId string) (*domain.Order, error)
+	DeliverOrder(ctx context.Context, orderId string, amount int) (*domain.Order, error)
+	Get(ctx context.Context, orderId string) (*domain.Order, error)
 }
 
 type IOrderRepository interface {
-	Save(ctx context.Context, board domain.Board) (*domain.Board, error)
-	Get(ctx context.Context, id string) (*domain.Board, error)
-	GetByName(ctx context.Context, name string) (*domain.Board, error)
-	GetByPlayer(ctx context.Context, playerId string) (*domain.Board, error)
-	Exist(ctx context.Context, name string) (bool, error)
-	DeleteAll(ctx context.Context)
+	Save(ctx context.Context, order domain.Order) (*domain.Order, error)
+	Get(ctx context.Context, orderId string) (*domain.Order, error)
 }
